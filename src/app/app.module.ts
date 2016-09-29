07ba3eddb5368e8b227903578b4c112976ef289b
Vierbergen Tim <Vierbergen.Tim@gmail.com>
2016-09-29 15:27:56 +0200

    Basic config update:
    Added material design with some components.
    Added google font.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdCoreModule } from '@angular2-material/core';
+import { MdCardModule } from '@angular2-material/card';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+import { MdListModule } from '@angular2-material/list';
+import { MdInputModule } from '@angular2-material/input';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdTabsModule } from '@angular2-material/tabs';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,9 +26,26 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MdCoreModule,
+    MdCardModule,
+    MdButtonModule,
+    MdIconModule,
+    MdListModule,
+    MdInputModule,
+    MdToolbarModule,
+    MdCheckboxModule,
+    MdProgressBarModule,
+    MdProgressCircleModule,
+    MdRadioModule,
+    MdSidenavModule,
+    MdTabsModule
+
   ],
-  providers: [],
+  providers: [
+    MdIconRegistry
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
